b0d9bf5d32771428d09b303da799d7458de7d2a7
gouedard-mathias <m-gouedard40500@hotmail.fr>
2021-02-02 07:58:29 +0100

    React 07 - Consommer une API avec React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import QuoteList from './components/QuoteList';
+import QuoteForm from './components/QuoteForm';
+import QuoteCard from './components/QuoteCard';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
+const initialQuoteCard = {
+    quote:"In theory, Communism works! In theory.",
+    character:"Homer Simpson",
+    image:"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+};
 
-    </div>
-  );
+class App extends React.Component
+{
+    constructor(props) {
+        super(props);
+        this.state = {
+            quoteCard: initialQuoteCard
+        };
+        this.getQuote = this.getQuote.bind(this);
+    }
+
+    getQuote() {
+        // Send the request
+        axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+            // Extract the DATA from the received response
+            .then(response => response.data)
+            // Use this data to update the state
+            .then(data => {
+                this.setState({
+                    quoteCard: data[0],
+                });
+                console.log(data);
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Navbar/>
+                <QuoteCard key={this.state.quoteCard.quote} quote={this.state.quoteCard.quote} image={this.state.quoteCard.image} character={this.state.quoteCard.character} />
+                <button type="button" onClick={this.getQuote}>Get Quote</button>
+                <QuoteForm/>
+                <QuoteList/>
+            </div>
+        );
+    }
 }
 
 export default App;
